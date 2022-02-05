3cc05b72934a47a54cc0df3ee3c431f3dac0b3bd
James Folk <jamesfolk@Jamess-MacBook-Pro.local>
2022-02-05 17:19:26 -0500

    shaking is being handled in the framework

--- MainApp/MainApp/ViewController.swift
+++ MainApp/MainApp/ViewController.swift
@@ -10,30 +10,7 @@ import FOLKFramework
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        self.becomeFirstResponder() // To get shake gesture
-        
-    }
     
-    // We are willing to become first responder to get shake motion
-    override var canBecomeFirstResponder: Bool {
-        get {
-            return true
-        }
-    }
-    
-    // Enable detection of shake motion
-    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        if motion == .motionShake {
-            var text = "FOLK View Explorer is ON"
-            if FOLKManager.shared.toggleExplorer() {
-                text = "FOLK View Explorer is OFF"
-            }
-            self.showToast(message: text, font: .systemFont(ofSize: 12.0))
-        }
-    }
 }
 
 
